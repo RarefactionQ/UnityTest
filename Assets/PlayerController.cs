4f674902aaa369cfc22bd1660fd736bc8f5150b5
Zander MacQuitty <myopia@Zanders-MacBook-Pro.local>
2017-07-28 17:57:54 -0700

    the good stuff

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speedX;
+	public float speedY;
+	public Rigidbody rb;
+	public bool jump;
+	public bool grounded;
+
+	// Update is called once per frame
+	void Update () {
+		
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * speedX;
+		if (Input.GetAxis ("Vertical") > 0) {
+			jump = true;
+			print ("jump");
+		}
+		transform.Translate (x, 0, 0);
+
+		if (jump && grounded) {
+			rb.AddForce (0, speedY, 0);
+			jump = false;
+			grounded = false;
+		}
+
+	}
+
+
+	void OnCollisionStay(Collision collisionInfo) {
+		if (collisionInfo.gameObject.tag.Contains ("Ground"))
+			print("OnCollisionStay");
+			grounded = true;
+	}
+			
+}
